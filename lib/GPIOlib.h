cefe89625e4728d349d8d3db1b9c81268178449d
Miau Shiauwe <miau.shiauwe@smail.nju.edu.cn>
2017-10-26 15:16:49 +0800

    Add some comments in the header file for understanding

--- lib/GPIOlib.h
+++ lib/GPIOlib.h
@@ -8,6 +8,7 @@ namespace GPIO
 {
 	int init();
 	
+	//direction is either FORWARD or BACKWARD. speed can be an integer ranging from 0 to 100.
 	int controlLeft(int direction,int speed);
 	int controlRight(int direction,int speed);
 	int stopLeft();
@@ -15,6 +16,7 @@ namespace GPIO
 	
 	int resetCounter();
 	void getCounter(int *countLeft,int *countRight);
+	//angle is available in the range of -90 to 90.
 	int turnTo(int angle);
 	
 	void delay(int milliseconds);
