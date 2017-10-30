20abb0f77e5ddbf3481d9bd4e9335d7a32fcfc40
Miau Shiauwe <miau.shiauwe@smail.nju.edu.cn>
2017-10-30 18:39:54 +0800

    Remove texts for unserstanding to Wiki pages

--- lib/GPIOlib.h
+++ lib/GPIOlib.h
@@ -8,7 +8,6 @@ namespace GPIO
 {
 	int init();
 	
-	//direction is either FORWARD or BACKWARD. speed can be an integer ranging from 0 to 100.
 	int controlLeft(int direction,int speed);
 	int controlRight(int direction,int speed);
 	int stopLeft();
@@ -16,7 +15,6 @@ namespace GPIO
 	
 	int resetCounter();
 	void getCounter(int *countLeft,int *countRight);
-	//angle is available in the range of -90 to 90.
 	int turnTo(int angle);
 	
 	void delay(int milliseconds);
